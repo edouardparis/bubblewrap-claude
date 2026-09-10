41a38967824d6ca6ba094ce51021f9c635bd8844
edouardparis <6933020+edouardparis@users.noreply.github.com>
2026-09-10 02:38:46 +0000

    chore: Update claude-code to v2.1.267

--- lib/sandbox/claude-package.nix
+++ lib/sandbox/claude-package.nix
@@ -1,5 +1,5 @@
 {pkgs}: let
-  version = "2.1.266";
+  version = "2.1.267";
 
   system = pkgs.stdenv.hostPlatform.system;
   platform =
@@ -11,8 +11,8 @@
     } or (throw "Unsupported system: ${system}");
 
   hashes = {
-    "linux-x64" = "sha256-GYQnBemJOT/Ok2gE320qsDSGDiS4+IgDV5gdh//YP6w=";
-    "linux-arm64" = "sha256-bkTF08l++qHuNauN2HWpB2gXB+XMT2yg2j0TqRe7zts=";
+    "linux-x64" = "sha256-A5nHk/9XHVlG75I9gLTzMNBaxLaEKmsHdUaPXTiUA8A=";
+    "linux-arm64" = "sha256-ImpOAJV0BEoYv1SV8SeAayob/L8ls8AWCHBfr+6V/vs=";
   };
 
   claudeBinary = pkgs.fetchurl {
